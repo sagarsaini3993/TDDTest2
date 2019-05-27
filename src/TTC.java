82bee34f0d92a8dc8c5109c2ee351b49f8298636
sagarsaini3993 <39058650+sagarsaini3993@users.noreply.github.com>
2019-05-27 10:38:25 -0400

    R1-Red

--- src/TTC.java
+++ src/TTC.java
@@ -1,4 +1,12 @@
 
 public class TTC {
+	
+	
+	
+	public double calculateTotal(String[] from, String[] to)
+	{
+		return 0;
+	}
+	
 
 }
--- test/TTCTest.java
+++ test/TTCTest.java
@@ -8,10 +8,21 @@ public class TTCTest {
 	@Before
 	public void setUp() throws Exception {
 	}
+	
+	
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testZone1() {
+		
+		
+		TTC t = new TTC();
+		String[] from = {"Leslie"};
+		String[] to = {"DonMills"};
+		double output = t.calculateTotal(from, to);
+		assertEquals("$2.50", output);
+		
+		
 	}
 
 }
