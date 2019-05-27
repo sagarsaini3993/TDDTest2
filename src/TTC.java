8813d93627ab51843e75c5f811cf117ba9bebeee
sagarsaini3993 <39058650+sagarsaini3993@users.noreply.github.com>
2019-05-27 11:55:52 -0400

    R3-Refactor

--- src/TTC.java
+++ src/TTC.java
@@ -1,4 +1,6 @@
 
+import java.util.Arrays;
+
 public class TTC {
 	
 	
@@ -10,21 +12,36 @@ public class TTC {
 		String[] Z1 = {"DonMills","Leslie"};
 		String[] Z2 = {"Sheppard","Finch"};
 		
-		if(from[0] == Z1[1] && to[0] == Z1[0]) {
-			
+		
+		
+		
+		boolean fromZone1 = Arrays.stream(Z1).anyMatch(from[0]::equals);
+		boolean toZone1 = Arrays.stream(Z1).anyMatch(to[0]::equals);
+
+		boolean fromZone2 = Arrays.stream(Z2).anyMatch(from[0]::equals);
+		boolean toZone2 = Arrays.stream(Z2).anyMatch(to[0]::equals);
+		
+		if(fromZone1 == true && toZone1 == true)
+		{
 			return 2.5;
-			
 		}
 		
-		else if(from[0] == Z2[0] && to[0] == Z2[1]) {
-		
-			return 3.00;
-		}
-		
-		else if(from[0] == Z1[0] && to[0] == Z2[1])
+		else if(fromZone2 == true && toZone2 == true)
 		{
 			return 3.00;
 		}
+		
+		else if(fromZone1 == true && toZone2 == true)
+		{
+			return 3.00;
+		}
+		
+		
+		
+		
+		
+		
+	
 		
 		return 0;
 	}
