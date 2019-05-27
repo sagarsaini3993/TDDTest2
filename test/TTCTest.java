89cc797e9a84658a12452694c34c785bcdae15c7
sagarsaini3993 <39058650+sagarsaini3993@users.noreply.github.com>
2019-05-27 12:03:07 -0400

    R4-Red

--- test/TTCTest.java
+++ test/TTCTest.java
@@ -45,5 +45,27 @@ public class TTCTest {
 		assertEquals("3.0", String.valueOf(actualOutput));
 
 	}
+	
+	@Test
+	public void moreThanOneTrip() {
+		
+		TTC t = new TTC();
+		String[] from = {"Finch"};
+		String[] to = {"Sheppard"};
+		double actualOutput = t.calculateTotal(from, to);
+		assertEquals("3.0", String.valueOf(actualOutput));
+		
+		
+		String[] from1 = {"Leslie"};
+		String[] to1 = {"DonMills"};
+		
+		double actualOutput1 = t.calculateTotal(from1, to1);
+		assertEquals("2.5", String.valueOf(actualOutput1));
+		
+		double totalFare = actualOutput + actualOutput1;
+		assertEquals("5.50", totalFare);
+		
+
+	}
 
 }
